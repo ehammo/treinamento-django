a6cd874f916f46795647362b880b6bc7f85188fb
ehammo <ehammo@cin.ufpe.br>
2017-12-02 22:20:14 -0200

    fixexd to string of models

--- core/models.py
+++ core/models.py
@@ -8,9 +8,15 @@ class Livro(models.Model):
     titulo = models.CharField(max_length=50)
     editora = models.ForeignKey('Editora')
 
+    def __str__(self):
+        return self.titulo
+
 class Editora(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
     nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
 
 class User(AbstractUser):
     pass
